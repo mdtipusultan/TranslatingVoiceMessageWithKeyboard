10f8fe0e2e6b1130ac9ca85a305c92196edbec17
Md. Tipu <tipu.diu.cse@gmail.com>
2026-03-30 01:26:48 +0600

    project created
    project created

---
+++ TranslatingVoiceMessage/TranslatingVoiceMessage/TranslatingVoiceMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatingVoiceMessageApp.swift
+//  TranslatingVoiceMessage
+//
+//  Created by MacBook Pro M1 Pro on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct TranslatingVoiceMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
